8454a19bac047d51527c695e89c50c9e848159f4
vv.golovkov <vv.golovkov@gmail.com>
2025-11-23 16:45:40 +0200

    initial commit of cicd

--- x_run/run.sh
+++ x_run/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
 
+# temporary values
 export PROFILE=dev
+export POD_NAME=pod
+export GIT_COMMIT=$(git rev-parse HEAD)
 
 docker compose up -d cs-consul cs-vault
 echo "sleeping 2s..."
 sleep 2
 ./x_config/load-kv.sh
-docker compose up --build -d
+#docker compose up --build -d
+docker compose up --build -d --scale m1-service=2 --scale m3-service=2
 docker compose logs -f
