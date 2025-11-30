63fa7535ab2497f6ad21bb9a390a6ea0b1704527
vv.golovkov <vv.golovkov@gmail.com>
2025-11-30 15:46:41 +0200

    k8s change 12

--- m1-service/src/main/java/com/home/m1service/task/config/CsConfigurationPojo.java
+++ m1-service/src/main/java/com/home/m1service/task/config/CsConfigurationPojo.java
@@ -1,8 +1,13 @@
 package com.home.m1service.task.config;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Optional;
 
 @Data
 @RefreshScope //needed for Vault (runtime update via actuator)
@@ -13,4 +18,10 @@ public class CsConfigurationPojo { //do not use RECORD, as @RefreshScope creates
     private String mType;
     private String m2ServiceUrl;
     private String m3ServiceUrl;
+
+    public String getmPass() {
+        return Optional.ofNullable(mPass)
+                .map(p -> new String(Base64.getDecoder().decode(p), StandardCharsets.UTF_8)).orElse(StringUtils.EMPTY);
+        //return new String(Base64.getDecoder().decode(mPass), StandardCharsets.UTF_8);
+    }
 }
