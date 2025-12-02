aedcf9f35606c8548c99e4b636070161a9950819
vv.golovkov <vv.golovkov@gmail.com>
2025-12-02 16:14:49 +0200

    move k8s config to special ConfigMap (done)

--- m1-service/src/main/java/com/home/m1service/task/config/CsConfigurationPojo1.java
+++ m1-service/src/main/java/com/home/m1service/task/config/CsConfigurationPojo1.java
@@ -20,16 +20,20 @@ public class CsConfigurationPojo1 { //do not use RECORD, as @RefreshScope create
     private String m2ServiceUrl;
     private String m3ServiceUrl;
 
-//    public void setMPass(String mPass) {
-//        log.info("M1.setMPass: Lombok has triggered for mPass-change [old={}; new={}]", this.mPass, mPass);
-//        if (mPass != null) {
-//            this.mPass = new String(Base64.getDecoder().decode(mPass), StandardCharsets.UTF_8);
-//        }
-//    }
+    /* -> option-1:
+    public void setMPass(String mPass) {
+        log.info("M1.setMPass: Lombok has triggered for mPass-change [old={}; new={}]", this.mPass, mPass);
+        if (mPass != null) {
+            this.mPass = new String(Base64.getDecoder().decode(mPass), StandardCharsets.UTF_8);
+        }
+    }
+    */
 
+    //option-2: triggered only once (on startup). The rest time, @RefreshScope uses ConfigurationRegistry1.onRefresh()
+    //May be it makes sense to move it to ConfigurationRegistry1 class and leave this DTO clear.
     @PostConstruct
     private void decodeBase64() {
-        log.info("M1.decodeBase64: @PostConstruct [old={}; new={}]", this.mPass, mPass);
+        log.info("M1.decodeBase64: @PostConstruct [{}]", mPass);
         if (mPass != null) {
             mPass = new String(Base64.getDecoder().decode(mPass), StandardCharsets.UTF_8);
         }
